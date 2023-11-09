77a4d7ef7f2f57f8fac27f9fb6cc2dc1bcb6a310
LUFFYTAROOOOO <149264146+LUFFYTAROOOOO@users.noreply.github.com>
2023-11-09 03:53:41 +0000

    comparision

---
+++ practice/comparision.js
@@ -0,0 +1,19 @@
+
+// console.log(null==0);
+// console.log(null>0);
+// console.log(null>=0); //comparision operators coverts to anumber and treated as  0 ,thus n>=0 is true  
+
+
+// console.log(undefined<0);
+// console.log(undefined==0);
+// console.log(undefined<=0);
+
+
+// strict check(===)
+
+// checks the data type also 
+ 
+// console.log("2"===1);  ans :false
+
+
+
