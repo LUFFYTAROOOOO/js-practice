3f60cad6a60d5cccfc623fcc0205c8c13b2797f8
LUFFYTAROOOOO <149264146+LUFFYTAROOOOO@users.noreply.github.com>
2023-11-10 03:38:21 +0000

    string intro and prototype

---
+++ practice/string.js
@@ -0,0 +1,48 @@
+const name = "ayush"
+const repocount = 100
+value = 12
+
+// console.log ( name + repocount + value);
+
+// console.log(`my name is ${name} and my repocoun is ${repocount}`);
+
+let names = new String("ayush")
+// console.log(names);
+// console.log(typeof nmaes);
+
+// trim 
+
+const newstrinf = "   ayush   "
+// console.log(newstrinf);
+// console.log(newstrinf.trim());
+
+const url = "https://ayushpanchal%20hbf"
+// console.log(url.replace("%20","-"));
+
+const array1 =[5,6,3,78,24];
+let index = 2
+// console.log(`An index of${index} returns ${array1.at(index)}`);
+
+//repeate
+const ayush = "hello ";
+// console.log(`i feel ${ayush.repeat(3)}`);
+
+//replaceall
+const n1 = " elephant are huge"
+// console.log(n1.replaceAll('elephant','dianosour'));
+
+// startwith
+const n2 = 'staurday is party night'
+// console.log(n2.startsWith('sta')); checks if the string starts with the provided  starting letters! and return true or false
+
+// upper/lower case 
+n3 = 'ayush'
+// console.log(n3.toUpperCase());
+n4 ='AYUSH'
+console.log(n3.toLowerCase());
+
+// value of
+const n5 = new String('ayush')
+// console.log(n5);
+// console.log(n5.valueOf());
+ 
