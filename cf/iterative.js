ac7d3331de8306685c0e8c185a7d90a670c93755
LUFFYTAROOOOO <149264146+LUFFYTAROOOOO@users.noreply.github.com>
2023-11-25 14:22:56 +0000

    basic loop

---
+++ cf/iterative.js
@@ -0,0 +1,24 @@
+
+//for loops
+
+for(let i=0;i<=10;i++){
+    // console.log(`this is outer loop${i}`);
+    for(let j=0;j<=10;j++){
+        // console.log(`this is inner looop${j}`);
+    }
+}
+
+
+// while and do while loops
+let i=1;
+while(i<10){
+   console.log(i);
+    // i= i+2;
+    i++;
+}
+
+
+do{
+    console.log(i);
+    i++;
+}while(i<100);
