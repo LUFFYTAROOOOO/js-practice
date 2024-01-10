e67859a66c2a8c875a6ca5f88457001e5ee90543
LUFFYTAROOOOO <149264146+LUFFYTAROOOOO@users.noreply.github.com>
2024-01-10 10:24:34 +0000

    revise

---
+++ newarrays.js
@@ -0,0 +1,21 @@
+const arr1 = [1,2,3,5,6,4]
+// console.log(arr1[0]);
+
+const arr2 = ["ayuhs","raj","mayuri"]
+arr2.push(2)
+arr2.unshift(0)
+arr2.pop()
+arr2.shift()
+// console.log(arr2);
+
+const joinarr = arr1.join()
+// console.log(joinarr);
+
+
+// splice
+
+// replaceses one element from the array 
+
+const newarr1 = ["ayush","mayuri","mansi","shruti","dev"]
+newarr1.splice(1,1,'patil')
+// console.log(newarr1);
